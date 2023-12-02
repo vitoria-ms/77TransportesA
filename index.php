0d524d164515a498f8fdc466944d6dfe23f82b7f
Maria Eduarda <mariaeduardalimatias@gmail.com>
2023-12-02 15:42:42 -0300

    rotas atualizadas

--- index.php
+++ index.php
@@ -4,3 +4,18 @@ declare(strict_types=1);
 
 require __DIR__ . "/vendor/autoload.php";
 
+$url = $_GET['url'] ?? "/";
+switch ($url) {
+    case "/":
+        echo "Página Inicial";
+        break;
+        case "login":
+            echo "Página de Login";
+            break;
+            case "cadastro":
+                echo "Página de Cadastro";
+                break;
+    default:
+    echo "404 - Página não Encontrada";
+        break;
+}
